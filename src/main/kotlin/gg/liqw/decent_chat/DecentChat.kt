a0fb0328d7cff426cc3c5a6ecd2fc3a35ad23f99
Liam Wahlberg <liam.wahlberg07@gmail.com>
2025-09-21 17:51:24 +0200

    initial new paper plugin

---
+++ src/main/kotlin/gg/liqw/decent_chat/DecentChat.kt
@@ -0,0 +1,14 @@
+package gg.liqw.decent_chat
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DecentChat : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
